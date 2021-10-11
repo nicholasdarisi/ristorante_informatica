fa3919b0272b8f87eed4edb3df0c864cff75735c
LAPTOP-4JS04K2V\gasar <g.a.sartoretto@gmail.com>
2021-10-11 13:03:02 +0200

    Inizio grafica

--- progetto_info_ristorante/src/View/Grafica.java
+++ progetto_info_ristorante/src/View/Grafica.java
@@ -1,43 +1,21 @@
 package View;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 
 public class Grafica {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Grafica window = new Grafica();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
+	
 	public Grafica() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+
+	public void mostra() {
+		frame.setVisible(true);
+	}
+	
+	
 
 }
