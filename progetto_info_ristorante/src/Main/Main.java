7d5e52baceeca7b8f5f65bd6c3631678d0bf2951
LAPTOP-4JS04K2V\gasar <g.a.sartoretto@gmail.com>
2021-10-12 12:55:22 +0200

    commenti per ruiza

--- progetto_info_ristorante/src/Main/Main.java
+++ progetto_info_ristorante/src/Main/Main.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.io.Serializable;
+
 import Controller.Controller;
 import Model.Amministrazione;
 import Model.Cameriere;
@@ -22,3 +24,60 @@ public class Main {
 	}
 
 }
+
+/*MAIN
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Scrittura di oggetti
+		try {
+			Persona pout = new Persona ("Alessio", "Sartoretto");
+			FileOutputStream fos = new FileOutputStream("elenco.dat");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(pout);
+			oos.flush();
+			fos.close();
+		} catch (Exception e) {}
+		
+		//Lettura di oggetti
+		try {
+			FileInputStream fis = new FileInputStream("elenco.dat");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Persona pin = (Persona) ois.readObject();
+			fis.close();
+			System.out.println(pin.toString());
+		} catch (Exception e) {}
+		
+	}
+
+}
+*/
+
+/*OGGETTO
+import java.io.Serializable;
+
+public class Persona implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private String nome;
+	private String cognome;
+	
+	public Persona(String nome, String cognome) {
+		this.nome = nome;
+		this.cognome = cognome;
+	}
+	
+	@Override
+	public String toString() {
+		return nome + " " + cognome;
+	}
+	
+}
+
+ */
