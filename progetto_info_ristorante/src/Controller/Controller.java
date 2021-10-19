efa8eaea915f402e26c55a01ac9e327de3a1c535
nicholasdarisi <nicholas.darisi@gmail.com>
2021-10-19 12:45:59 +0200

    controller

--- progetto_info_ristorante/src/Controller/Controller.java
+++ progetto_info_ristorante/src/Controller/Controller.java
@@ -17,6 +17,7 @@ public class Controller implements ActionListener{
 	Cassa cas;
 	Ordine o;
 	Grafica g;
+	String check;
 	
 	public Controller(Amministrazione a, Cameriere cam, Cucina cuc, Cassa cas, Ordine o, Grafica g) {
 		this.a = a;
@@ -36,19 +37,18 @@ public class Controller implements ActionListener{
 		// TODO Auto-generated method stub
 		//ordina
 		if (e.getSource() == g.getButton(0)) { 
-			
+			//check = cam.receiveorder(g.ordine());
+			//informazioni.setText(check);
 			
 		}
 		
 		//reset
 		if (e.getSource() == g.getButton(1)) {
-			
-			
+			//g.reset();
 		}
 		
 		//cucina
 		if (e.getSource() == g.getButton(2)) {
-	
 			
 		}
 		
